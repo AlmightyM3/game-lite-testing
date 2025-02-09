ed2b16bd4eb2cf4b574d0397382bc78918df08fb
AlmightyM3 <143669006+AlmightyM3@users.noreply.github.com>
2025-02-08 21:45:11 -0500

    Tested calling cpp functions

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let num = 1000000
+let num = 100000000
 // let big = Buffer.fromArray([97, 96, 93, 87, 80, 76, 77, 76, 80, 90, 95, 95, 95, 94, 93, 93, 95, 95, 96, 95, 92, 90, 87, 83, 79, 75, 74, 75, 79, 82, 83, 83, 84, 87, 92, 98, 100, 100, 108, 116, 120, 123, 124, 124, 123, 124, 124, 124, 123, 124, 124, 124, 123, 123, 124, 124, 124, 124, 124, 124, 123, 124, 124, 123, 123, 124, 123, 124, 123, 124, 124, 124, 123, 123, 123, 123, 123, 123, 124, 124, 124, 124, 122, 119, 118, 115, 114, 112, 109, 106, 102, 97, 93, 89, 84, 80, 79, 79, 75, 73, 69, 65, 59, 55, 54, 53, 54, 57, 55, 53, 53, 54, 53, 52, 57, 64, 70, 73, 78, 81, 84, 88, 91, 93, 93, 93, 93, 95, 97, 98, 99, 98, 97, 97, 97, 97, 95, 94, 98, 100, 99, 95, 91, 84, 80, 80, 79, 79, 79, 79, 79, 80, 79, 74, 66, 56, 50, 46, 42, 38, 34, 29, 30, 34, 35, 36, 32, 27, 27, 29, 29, 29, 30, 29, 28, 27, 27, 27, 28, 31, 35, 37, 36, 32, 28, 28, 31, 33, 35, 35, 34, 34, 31, 27, 27, 27, 25, 25, 28, 28, 28, 28, 29, 29, 29, 29, 28, 28, 29, 29, 31, 35, 36, 34, 31, 29, 26, 23, 17, 16, 21, 27, 33, 40, 45, 50, 53, 54, 57, 57, 53, 53, 53, 54, 55, 57, 58, 61, 65, 68, 69, 69, 69, 69, 72, 76, 79, 84, 87, 91, 94, 97, 98, 98, 98, 97,])
 // console.logValue("big.toHex()", big.toHex())
 
@@ -8,9 +8,38 @@ game.onPaint(() => {
 
     testBuffersCreate()
     testBuffersGet()
+    // testCppMethods()
 
     console.log("End")
 })
+
+function pack(x: number, y: number) {
+    return (Math.clamp(-30000, 30000, x | 0) & 0xffff) | (Math.clamp(-30000, 30000, y | 0) << 16)
+}
+//% shim=ImageMethods::_fillRect
+function _fillRect(img: Image, xy: number, wh: number, c: color): void { }
+function testCppMethods() {
+    // _fillRect is 7.5% faster then image.screenImage().fillRect and 6.2% faster then helpers.imageFillRect
+    
+    console.log("Testing methods of drawing vertical lines with cpp")
+    let startTime = control.millis()
+    for (let i = 0; i < num; i++) {
+        image.screenImage().fillRect(10, 10, 1, 100, 5)
+    }
+    console.logValue("image.screenImage().fillRect", control.millis() - startTime)
+
+    startTime = control.millis()
+    for (let i = 0; i < num; i++) {
+        helpers.imageFillRect(image.screenImage(), 20, 10, 1, 100, 5)
+    }
+    console.logValue("helpers.imageFillRect", control.millis() - startTime)
+
+    startTime = control.millis()
+    for (let i = 0; i < num; i++) {
+        _fillRect(image.screenImage(), pack(30, 10), pack(1, 100), 5)
+    }
+    console.logValue("_fillRect", control.millis() - startTime)
+}
 
 function testBuffersCreate() {
     console.log("Testing buffer creation against arrays")
